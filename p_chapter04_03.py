e46343fcb0b8e45a1c7a46195010da06e1822fd1
Baek Jong Min <guentc1@naver.com>
2021-07-04 23:44:04 +0900

    집에서 와서 작업한 내용

--- p_chapter04_03.py
+++ p_chapter04_03.py
@@ -8,4 +8,46 @@
 # 해시테이블
 # key의 value를 저장하는 구조
 # 파이썬 dict 해쉬 테이블 예제
-# 키 값의 연산 결과에 따라 직접 접근이 가능한 구조조
+# 키 값의 연산 결과에 따라 직접 접근이 가능한 구조
+# key 값을 해싱 함수 -> 해쉬 주소 -> key에 대한 value 참조
+
+# Dict 구조
+# print(__builtins__.__dict__)
+
+# Hash 값 확인
+# 불변현 값만 들어가야한다.
+
+# 증명
+t1 = (10, 20, (30, 40, 50))  # tuple은 immutable
+# t2 = (10, 20, [30, 40, 50])  # list는 mutable 값이기 때문에 hash 함수를 사용할 수 없다.
+
+print(hash(t1))
+# print(hash(t2))
+
+# Dict Setdefault 예제
+source = (('k1', 'val1'),
+          ('k1', 'val2'),
+          ('k2', 'val3'),
+          ('k2', 'val4'),
+          ('k2', 'val5'))
+
+new_dict1 = {}
+new_dict2 = {}
+
+# No use Setdefault
+for k, v in source:
+    if k in new_dict1:
+        new_dict1[k].append(v)
+    else:
+        new_dict1[k] = [v]
+
+print(new_dict1)
+
+# Use Setdefault
+for k, v in source:
+    new_dict2.setdefault(k, []).append(v)
+
+print(new_dict2)
+
+# 주의
+new_dict3 = {k: v for k, v in source}
